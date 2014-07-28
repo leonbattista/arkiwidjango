cdcf7904e20dccd32f2399f9e492da62efa7b532
Joel Baur <privat.joel@gmail.com>
2014-07-28 16:57:14 +0200

    secret key is secret now

--- arkiwi/settings.py
+++ arkiwi/settings.py
@@ -1,15 +1,11 @@
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
 # Import the private settings that are in gitignore
 from local.settings_private import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-
 
 TEMPLATE_DEBUG = True
 

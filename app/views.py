0ef4f72c775a17ebccaf2b73d39669ddd79e9eda
Pierre Chevremont <pierre.chevremont@gmail.com>
2014-07-29 18:34:54 +0200

    Random messing out with experiments

--- app/views.py
+++ app/views.py
@@ -1,11 +1,22 @@
 from django.views.generic import View
-from djangular.views.mixins import JSONResponseMixin, allow_remote_invocation
 from django.shortcuts import render
-from djangular.core.urlresolvers import get_all_remote_methods    
+from django.http import HttpResponse
+import json
 
 def index(request):
     
     
     return render(request, 'app/base.html')
+    
+def test(request):
+    
+    params = request.GET
+    testList = range(int(params['b']))
+    if params['a'] == "OK":
+        data = {"reponse":"cool", "testB":testList}
+    else:
+        data = {"reponse":"cool aussi en fait", "testB":testList}
+        
+    return HttpResponse(json.dumps(data), "application/json")
 
 

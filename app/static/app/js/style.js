69fad0dd06326bc8420b1f5762a32f89ab749a12
Joel Baur <privat.joel@gmail.com>
2014-07-29 18:38:09 +0200

    continuing on playing js css

---
+++ app/static/app/js/style.js
@@ -0,0 +1,45 @@
+//////////////////////////////// -- dynamic styles -- ///////////////////////////////////
+//get $scope of window width and change the width of a specific #id
+function elementWidth($scope) {
+
+    console.log("Window Width " + window.innerWidth);
+    console.log("Images per line " + Math.floor(window.innerWidth/400));
+
+    var 
+    Elemcolumn1 = document.getElementById('column1'),
+    Elemcolumn2 = document.getElementById('column2'),
+    Elemcolumn3 = document.getElementById('column3'),
+
+    scopeElemcolumn1 = angular.element(Elemcolumn1).scope();
+    //scopeElemcolumn2 = angular.element(Elemcolumn2).scope();
+    //scopeElemcolumn3 = angular.element(Elemcolumn3).scope();
+
+    if (window.innerWidth >= 1500) {
+    console.log("window bigger than 1500 = " + window.innerWidth);
+    //scopeElemcolumn1.width = 100;
+    $scope.column1Width = 100;
+    $scope.column2Width = 250;
+    $scope.column3Width = window.innerWidth - 405;
+	}
+	else {
+	console.log("window smaler than 1500 = " + window.innerWidth);
+    $scope.column1Width = 70;
+    $scope.column2Width = 150;
+    $scope.column3Width = window.innerWidth - 305;	
+	}	
+
+    scopeElemcolumn1.$apply(function() {
+        Elemcolumn1.style.width = $scope.column1Width.toString() + 'px';
+        Elemcolumn2.style.left = $scope.column1Width.toString() + 'px';
+        Elemcolumn2.style.width = $scope.column2Width.toString() + 'px';
+        Elemcolumn3.style.width = $scope.column3Width.toString() + 'px';
+        console.log("col. widths: " + Elemcolumn1.style.width + " " + Elemcolumn2.style.width + " " + Elemcolumn3.style.width);
+
+    });
+
+}
+//first call of elementWidth when the dom is loaded
+document.addEventListener("DOMContentLoaded", elementWidth, false);
+
+//calling elementWidth on resize event
+window.onresize = elementWidth;

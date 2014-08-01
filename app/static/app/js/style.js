9283d518ddb4ad7a6bb19535be981cacd6bb448c
Joel Baur <privat.joel@gmail.com>
2014-08-01 14:47:23 +0200

    details

--- app/static/app/js/style.js
+++ app/static/app/js/style.js
@@ -1,4 +1,4 @@
-jsApp.directive('resize', function ($window, menuVisibilityService) {
+jsApp.directive('arkiwiResize', function ($window, menuVisibilityService) {
     return function (scope, element) {
         var w = angular.element($window);
         scope.getWindowDimensions = function () {
@@ -10,12 +10,12 @@ jsApp.directive('resize', function ($window, menuVisibilityService) {
         scope.$watch(scope.getWindowDimensions, function (newValue) {
             scope.column1style = function () {
                 return {
-                    'width': (80) + 'px'
+                    'width': 80 + 'px'
                 };
             };
             scope.column2style = function () {
                 return {
-                    'width': (240) + 'px'
+                    'width': 240 + 'px'
                 };
             };
             scope.column3style = function () {
@@ -39,28 +39,26 @@ jsApp.directive('resize', function ($window, menuVisibilityService) {
 
 jsApp.service('menuVisibilityService', function() {
     this.menuVisibilityVar = false;
-
     this.setTrueTag = function() {
         this.menuVisibilityVar = true;
     };    
-
     this.setFalseTag = function() {
         this.menuVisibilityVar = false;
     };
 });
 
-
 jsApp.controller('MenuCtrl', function($scope, menuVisibilityService){
     $scope.menuShow = function(){
      menuVisibilityService.setTrueTag();
     }
-
     $scope.menuHide = function(){
      menuVisibilityService.setFalseTag();
     }
-
     $scope.menuVisibility = function(){
      return menuVisibilityService.menuVisibilityVar;
     }
+});
 
+jsApp.controller('SubmenuContentCtrl', function($scope){
+    var schubidu = '123';
 });

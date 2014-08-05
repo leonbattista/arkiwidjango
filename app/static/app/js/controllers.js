5fe345297ea624f01aeea45fa956b5737481e08d
Pierre Chevremont <pierre.chevremont@gmail.com>
2014-08-05 23:23:13 +0200

    Small bug correction

--- app/static/app/js/controllers.js
+++ app/static/app/js/controllers.js
@@ -8,22 +8,24 @@ app.controller('MenuCtrl', function($scope, $http, menuVisibilityService, AuthSe
    	
 	$scope.isLogged = false;
 	$scope.username = "";
+	
 	function updateIsLogged(newValue, oldValue) {
 		$scope.isLogged = newValue;
 		$scope.username = AuthService.getUsername();
-		console.log("update" + $scope.username + $scope.isLogged);		
 	}
 	
 	$scope.$watch(AuthService.checkLogin, updateIsLogged);
 	
 	$scope.menuShow = function(){
-     menuVisibilityService.setTrueTag();
-	 
-    }
+     menuVisibilityService.setTrueTag();	 
+    };
+	
     $scope.menuHide = function(){
      menuVisibilityService.setFalseTag();
-    }
+    };
+	
     $scope.menuVisibility = function(){
      return menuVisibilityService.menuVisibilityVar;
-    }
+    };
+	
 });
